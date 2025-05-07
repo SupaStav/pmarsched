2ab947f51d2882929bc5d8a4b63f83474edc8f35
Stavros Morrison <morrstavrostplz@gmail.com>
2025-05-06 21:34:02 -0700

    added some junk to the course previewer yk

--- src/course/courseview.js
+++ src/course/courseview.js
@@ -1,10 +1,19 @@
 import './courseview.css'
 
+function fetchCourses()
+{
+    return [{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},{'p': "This is a test", 'a':"of my course previewer"},];
+}
+
 function CourseView()
 {
+    const listItems = fetchCourses().map(i => <div className='listItem'><div className='courseTitle'><b>{i.p.toUpperCase()}</b> {i.a}</div><div className='addButton'></div></div>);
     return(
         <div className="courseView">
             <input name='Filter'/>
+            <div className='Classselect'>
+                <li>{listItems}</li>
+            </div>
         </div>
     )
 }
